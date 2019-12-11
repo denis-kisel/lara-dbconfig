0ca42ded28d43a7abf6a74e5172eafd8f67d9ccd
Denis <denis.kisel92@gmail.com>
2019-12-11 09:16:42 +0200

    fix model

--- src/SettingsModel.php
+++ src/SettingsModel.php
@@ -8,6 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class SettingsModel extends Model
 {
+    public function __construct(array $attributes = [])
+    {
+        $this->table = config('dbconfig.table', 'settings');
+        parent::__construct($attributes);
+    }
+
     public function getValueAttribute()
     {
         return unserialize($this->attributes['value']);
@@ -16,10 +22,5 @@ class SettingsModel extends Model
     public function setValueAttribute($value)
     {
         $this->attributes['value'] = serialize($value);
-    }
-
-    public function setTable($table)
-    {
-        return config('dbconfig.table', 'settings');
     }
 }
