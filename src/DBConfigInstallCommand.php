03232d519dc508c5f4c6fa8fe1c0119a120d341c
Denis <denis.kisel92@gmail.com>
2019-12-11 09:13:19 +0200

    fix migration

--- src/DBConfigInstallCommand.php
+++ src/DBConfigInstallCommand.php
@@ -5,6 +5,7 @@ namespace DenisKisel\DBConfig;
 
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Str;
 
 class DBConfigInstallCommand extends Command
 {
@@ -13,11 +14,12 @@ class DBConfigInstallCommand extends Command
     public function handle()
     {
         $tableName = config('dbconfig.table', 'settings');
-        $path = database_path('/migrations/' . date('Y_m_d_His')) . "_create_{$tableName}_table";
+        $path = database_path('/migrations/' . date('Y_m_d_His')) . "_create_{$tableName}_table.php";
         copy(__DIR__ . '/../resources/migration.stub', $path);
 
         $content = file_get_contents($path);
         $content = str_replace('{tableName}', $tableName, $content);
+        $content = str_replace('{className}', 'Create' . Str::studly($tableName) . 'Table', $content);
         file_put_contents($path, $content);
 
         $this->info("Migration for {$tableName} table is created!");
